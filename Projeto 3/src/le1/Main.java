e7cf730cba033cf5f18d96a6c2f800dfd51baca0
Sonael <sonaelneto@gmail.com>
2022-03-23 20:16:07 -0300

    first commit

---
+++ Projeto 3/src/le1/Main.java
@@ -0,0 +1,36 @@
+package le1;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import le1.plp.expressions1.Programa;
+import le1.plp.expressions1.expression.ExpDiv;
+import le1.plp.expressions1.expression.ExpMult;
+import le1.plp.expressions1.expression.ValorInteiro;
+import le1.plp.expressions1.parser.Exp1Parser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Testes usando as classes criadas. 
+		
+		
+		ValorInteiro valor1 = new ValorInteiro(30);
+		ValorInteiro valor2 = new ValorInteiro(10);
+
+	
+		//Expressão Mult
+		ExpMult expmult = new ExpMult(valor1, valor2);
+		Programa programa1 = new Programa(expmult);
+		programa1.executar();
+
+		
+		//expressão Div
+		ExpDiv expdiv = new ExpDiv(valor1, valor2);
+		Programa programa2 = new Programa(expdiv);
+		programa2.executar();
+		
+	}
+
+}
