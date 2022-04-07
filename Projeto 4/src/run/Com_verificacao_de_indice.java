ec24bc775fb6f792145a4e7b598c668715206bff
Sonael <sonaelneto@gmail.com>
2022-04-07 20:18:25 -0300

    Finalizado

---
+++ Projeto 4/src/run/Com_verificacao_de_indice.java
@@ -0,0 +1,88 @@
+package run;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class Com_verificacao_de_indice {
+
+	public static void main(String[] args) {
+		long tempoInicial = System.currentTimeMillis();
+		Random r = new Random();
+		int cont = 0;
+		
+		int[][] matrizA = new int[50][50];
+		int[][] matrizB = new int[matrizA.length][matrizA.length];
+		ArrayList<int[][]> matrizes = new ArrayList<int[][]>();
+		ArrayList<int[][]> Multmatrizes = new ArrayList<int[][]>();
+
+		
+		preencher(matrizA, matrizes, r);
+		
+		while(cont < 100) {
+		mult(matrizA, matrizB, Multmatrizes, matrizes, cont);
+		cont+=2;
+		}
+		
+		System.out.println("Tamanho do vetor das matrizes: "+ matrizes.size());
+		System.out.println("Tamanho do vetor das matrizes resultantes: "+ Multmatrizes.size());
+		
+		long tempoFinal = System.currentTimeMillis() - tempoInicial;
+		System.out.println("o metodo executou em " + tempoFinal+ "ms");
+	
+		
+	}
+	
+	//Faz a multiplicação das matrizes e adiciona dentro do array Multmatrizes
+	public static void mult(int[][] matrizA, int[][]matrizB, ArrayList<int[][]> Multmatrizes,ArrayList<int[][]> matrizes,int cont) {
+		matrizB = new int[matrizB.length][matrizB.length];
+		for (int i = 0; i < matrizB.length; i++) 
+		{
+			for (int j = 0; j < matrizB.length; j++) 
+			{
+				for (int k = 0; k < matrizB.length; k++) 
+				{
+					if(verificar(i, j, k, cont, matrizes)) {
+					matrizB[i][j] += matrizes.get(cont)[i][k] * matrizes.get(cont+1)[k][j];
+					}
+				}
+			}	
+		}
+		Multmatrizes.add(matrizB);
+	}
+	
+	//Preenche a matriz com numeros aleatorios e adiciona no array Matrizes.
+	public static void preencher(int[][] matrizA,ArrayList<int[][]> matrizes, Random r) {
+		for (int a = 0; a < 100; a++) {
+			matrizA = new int[matrizA.length][matrizA.length];
+			for (int i = 0; i < matrizA.length; i++) 
+			{
+				for (int j = 0; j < matrizA.length; j++) 
+				{
+					
+					matrizA[i][j] = r.nextInt(10);
+				}
+				
+			}
+			matrizes.add(matrizA);
+		}
+
+	}
+	
+	//Faz a verificação de cada item
+	public static boolean verificar(int i,int j,int k, int cont,ArrayList<int[][]> matrizes ) {
+		boolean result = true;
+		int test;
+		try {
+			test = matrizes.get(cont)[i][k];
+			test = matrizes.get(cont+1)[k][j];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Error");
+			result = false;
+		}
+		
+		return result;
+	}
+
+}
--- Projeto 4/src/run/Main.java
+++ Projeto 4/src/run/Main.java
@@ -1,76 +1,20 @@
 package run;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Random;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Random r = new Random();
-		ArrayList<Matriz> matrizes = new ArrayList<Matriz>();
-		ArrayList<Matriz> Multmatrizes = new ArrayList<Matriz>();
-		int[][] matrizC =new int[2][2];
-		Matriz matrizResul = new Matriz();
 		
-		for (int i = 0; i < 100; i++) {
-			Matriz matriz = new Matriz();
-			matriz.preencher();
-			matrizes.add(matriz);
-		}
-		
-		
-				
-		//Continuar multiplicação de matrizes
-		
-		
-		for (int a = 0; a < matrizes.size(); a = a+2) {
-			for (int i = 0; i < 2; i++) 
-			{
-				for (int j = 0; j < 2; j++) 
-				{
-					for (int k = 0; k < 2; k++)
-					{
-						matrizC[i][j] += (matrizes.get(a).getMatriz()[i][k]) * (matrizes.get(a+1).getMatriz()[k][j]);
-						
-					}
-				}
-			}
-			matrizResul.setMatriz(matrizC);
-			Multmatrizes.add(matrizResul);
-		}
-		
-
-		
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				System.out.print(matrizes.get(0).getMatriz()[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
+		System.out.println("Classe com sem Verificação de Indice:");
 		System.out.println();
-		
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				System.out.print(matrizes.get(1).getMatriz()[i][j]+" ");
-			}
-			System.out.println();
-		}
+		Sem_vericacao_de_indice.main(args);
+
+
 		System.out.println("\n");
-		for (int i = 0; i < 2; i++) 
-		{
-			for (int j = 0; j < 2; j++) 
-			{
-				for (int k = 0; k < 2; k++)
-				{
-					System.out.println((matrizes.get(0).getMatriz()[i][k]) * (matrizes.get(1).getMatriz()[k][j])+ " ");
-					
-				}
-				
-			}
-			System.out.println();
-		}
+		
+		
+		System.out.println("Classe com Verificação de Indice:");
+		System.out.println();
+		Com_verificacao_de_indice.main(args);
 	}
 
 }
