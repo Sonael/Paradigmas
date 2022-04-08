2681c85bcccc5a7d32dfb343679a80b6c5174435
Sonael <sonaelneto@gmail.com>
2022-04-07 22:25:07 -0300

    Finalizado

--- Projeto 4/src/run/Main.java
+++ Projeto 4/src/run/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Classe com sem Verificação de Indice:");
+		System.out.println("Classe sem Verificação de Indice:");
 		System.out.println();
 		Sem_vericacao_de_indice.main(args);
 
